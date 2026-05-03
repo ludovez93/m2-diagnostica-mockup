5d0c53eec5a76f318548da8bcd88fd2344960821
ludovez93 <ludovez93@users.noreply.github.com>
2026-05-03 08:56:14 +0200

    cleanup: rimuovi script integrazione temp

--- may-2026/concept-d/_i.js
+++
@@ -1,8 +0,0 @@
-const fs=require('fs'),p=require('path');
-const idx='index.html', vw='views/dashboard.html';
-let h=fs.readFileSync(idx,'utf8');
-let c=fs.readFileSync(vw,'utf8').trim().replace(/^<section[^>]*data-screen=["']?[^"'>]+["']?[^>]*>/,'').replace(/<\/section>\s*$/,'').trim();
-const re=new RegExp(`(<section[^>]*data-screen="dashboard"[^>]*>)([\s\S]*?)(</section>)`);
-const r=h.replace(re,`$1\n      ${c.split('\n').join('\n      ')}\n    $3`);
-if(r===h){console.error('NO MATCH');process.exit(1);}
-fs.writeFileSync(idx,r);console.log('OK',c.length);
